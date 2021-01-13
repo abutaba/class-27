9c0d1e65eb1dd71523a6e39495e5cba21fc18faa
abutaba <77039523+abutaba@users.noreply.github.com>
2021-01-13 14:57:42 +0530

    Add files via upload

---
+++ AngryBirdsStage2.5StudentActivity-master/Chain.js
@@ -0,0 +1,12 @@
+class chain {
+    constructor(bodyA, bodyB){
+    var options = {
+        bodyA : bodyA,
+        bodyB : bodyB,
+        stiffness:0.04,
+        length:10,
+      }
+      this.chain = Constraint.create(options);
+      World.add (world, this.chain)
+    }
+} 
